d14562f1d932d2851b17a8edae85b8bd041a939d
Kamal <kamalfarhat@gmail.com>
2023-08-19 16:54:42 +0800

    Commit bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,14 @@
 def bubble_sort(array)
+    for i in 0..array.length-2
+        for j in 0..array.length-2
+            if array[j] > array[j+1]
+                array[j], array[j+1] = array[j+1], array[j]
+            end
+        end
+    end
 
+return array
 end
+
+array = [10,9,8,7,6,5,4,3,2,1]
+p bubble_sort(array)
